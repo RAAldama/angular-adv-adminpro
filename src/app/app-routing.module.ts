a5c16b62621c0beecf88279bdc2f60eb7be1dced
Ricardo Aldama <ricardoaldama98@gmail.com>
2020-10-29 19:45:16 -0600

    Finalizado el copy/paste UwU

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graphic1Component } from './pages/graphic1/graphic1.component';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: PagesComponent, 
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graphic1', component: Graphic1Component },
+      { path: '', pathMatch:'full', redirectTo: '/dashboard' },
+    ]
+  },
+  
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  { path: '**', component: NoPageFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
